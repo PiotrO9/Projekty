10b93b42edf0791a573613cf2936a6f85bc9b334
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-15 18:19:21 +0200

    Nowy projekt

---
+++ spoj.com_RNO_DOD/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoj.com_RNO_DOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1;
+
+            int test = text1.
+
+            for (int i = 0; i < number1;i++)
+            {
+                string label1 = Console.ReadLine();
+
+                string[] array1 = label1.Split(' ');
+
+            }
+
+
+        }
+    }
+}
