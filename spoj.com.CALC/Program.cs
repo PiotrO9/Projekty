d92badaaf8f5d457e3dd561cbd54a9897bc95055
praktyki <praktyki@mta.net.pl>
2020-07-15 13:59:19 +0200

    Dodaj pliki projektów.

---
+++ spoj.com.CALC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoj.com.CALC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            var userInputText = Console.ReadLine();
+
+            int convertedNumber;
+            int mnoznikOutput;
+
+            if (TryParseUserDates(userInputText, out convertedNumber, "Użytkwnik nie wprowadził liczby") == false)
+                return;
+
+                string mnoznikInput = Console.ReadLine();
+
+            if (TryParseUserDates( mnoznikInput, out mnoznikOutput, "Użytkwnik nie wprowadził poprawnego mnożnika") == false)
+                return;
+
+            Console.WriteLine(convertedNumber * mnoznikOutput);
+            Console.ReadLine();
+        }
+
+        private static bool TryParseUserDates(string userText,out int userNumber, string userComunicate)
+        {
+            if (!int.TryParse(userText, out userNumber))
+            {
+                Console.WriteLine(userComunicate);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ spoj.com.CALC2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoj.com.CALC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInputText = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(userInputText))
+            {
+                Console.WriteLine("Pętla zaczyna się");
+                userInputText = Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
