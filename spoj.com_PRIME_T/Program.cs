adf9fb925cea2d0f7f8aa82a810c2521ccd6203b
praktyki <praktyki@MTA-27>
2020-07-15 16:03:55 +0200

    Skończone

--- spoj.com_PRIME_T/Program.cs
+++ spoj.com_PRIME_T/Program.cs
@@ -10,26 +10,42 @@ namespace spoj.com_PRIME_T
     {
         static void Main(string[] args)
         {
-            Console.Write("Podaj liczbęi sprawdź czy jest liczbą pierwszą: ");
+            for (; ; )
+            {
+                Console.Write("Podaj liczbę i sprawdź czy jest liczbą pierwszą: ");
 
                 string testInput = Console.ReadLine();
-            int liczba = int.Parse(testInput);
+                int liczba = int.Parse(testInput);
 
-               
+                Console.Clear();
+
+                if (pierwsza(liczba) == true)
+                {
+                    Console.Write("To liczba pierwsza \n");
+                }
+                else
+                {
+                    Console.Write("To nie liczba pierwsza \n");
+                }
+
+                
+
+            }
 
         }
 
-        bool pierwsza(int number)
+        static bool pierwsza(int number)
         {
             if(number<2)
             { return false; }
             
             for(int i=2;i*i<=number;i++)
             {
-                return true;
+                if(number%i==0)
+                return false;
             }
 
-            return false;
+            return true;
         }
     }
 }
