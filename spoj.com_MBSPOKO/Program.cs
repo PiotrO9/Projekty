5c7b10b23471307323817d4ac5f45276d8a249f9
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-18 09:26:25 +0200

    Projekt skończony

--- spoj.com_MBSPOKO/Program.cs
+++ spoj.com_MBSPOKO/Program.cs
@@ -12,8 +12,49 @@ namespace spoj.com_MBSPOKO
         {
             int loops = int.Parse(Console.ReadLine());
 
+            for(int i=0; i<loops; i++)
+            {
+                int numberInput = int.Parse(Console.ReadLine());
 
-
+                if(divisionBy3(numberInput) == true)
+                {
+                    if (divisionBy5(numberInput) == true)
+                    {
+                        Console.WriteLine("SPOKOKOKO");
+                    }
+                    else
+                    {
+                        Console.WriteLine("KOKO");
+                    }
+                }
+                else if(divisionBy5(numberInput) == true && divisionBy3(numberInput) == false)
+                {
+                    Console.WriteLine("SPOKO");
+                }
+                else 
+                {
+                    Console.WriteLine(numberInput);
+                }
+            }
         }
+
+        static bool divisionBy3 (int number)
+        {
+            if(number % 3 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool divisionBy5(int number)
+        {
+            if (number % 5 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
