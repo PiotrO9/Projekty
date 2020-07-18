0e2701da3d7cafab78dd10fb2715ca27586a2879
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-18 09:12:44 +0200

    Nowy prjoekt

---
+++ spoj.com_MBSPOKO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoj.com_MBSPOKO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loops = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
