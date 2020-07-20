6ef02c3f52fdee7313888c79a23568102f7d65db
praktyki <praktyki@MTA-27>
2020-07-20 16:33:04 +0200

    Projekt skończony

---
+++ w3resource.com_exercise_array_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w3resource.com_exercise_array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loops = int.Parse(Console.ReadLine());
+
+            int[] array1 = new int[loops];
+
+            for (int i = 0; i < loops; i++)
+            {
+                if (i == 0)
+                {
+                    array1[i] = 1;
+                }
+                else
+                {
+                    array1[i] = i;
+                }
+
+            }
+
+            Console.WriteLine("----------------------");
+
+            for (int i = 0; i < loops; i++)
+            {
+                Console.WriteLine(array1[i] + " ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ w3resource.com_exercise_array_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("w3resource.com_exercise_array_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("w3resource.com_exercise_array_1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("195d46a8-6798-4d72-93d9-96e2725740f2")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
