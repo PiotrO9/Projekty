3508bf108415a463165fd9075716a64394801225
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-21 18:19:08 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loops = int.Parse(Console.ReadLine());
+
+            int[] array1 = new int[loops];
+
+            for (int i = 0; i < loops; i++)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = array1.Length; i > 0; i--)
+            {
+                Console.WriteLine(array1[i - 1]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
