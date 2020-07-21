879ed52770bbda015b0069e86747e9645c1a5d30
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-21 17:06:47 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int number = int.Parse(input);
+
+            int[] array1 = new int[number];
+
+            for(int i=0;i<number;i++)
+            {
+                if(i == 0)
+                {
+                    array1[i] = 1;
+                }
+                else
+                {
+                    array1[i] = i;
+                }
+            }
+
+            for(int i = 0;i<number;i++)
+            {
+                Console.WriteLine(array1[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
