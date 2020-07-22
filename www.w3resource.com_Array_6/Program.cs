f049d7beafe0be95e0558a1fa458375c3a73db77
praktyki <praktyki@MTA-27>
2020-07-22 13:12:45 +0200

    Zadanie skończone

--- www.w3resource.com_Array_6/Program.cs
+++ www.w3resource.com_Array_6/Program.cs
@@ -13,7 +13,48 @@ namespace www.w3resource.com_Array_6
         {
             int[] arrayFromUser = InteractiveWithUser.GetIntArray();
 
+            int NumberOfLoops = arrayFromUser.Length;
 
+            if (NumberOfLoops <= 0)
+            {
+                Console.WriteLine("Brak tablicy od użytkownika");
+            }
+            else
+            {
+                bool[] differentValuesOfArray = new bool[NumberOfLoops];
+                for (int i = 0; i < NumberOfLoops; i++)
+                {
+                    if (differentValuesOfArray[i] == true)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < NumberOfLoops; j++)
+                        {
+                            if (i == j)
+                                continue;
+                            if (arrayFromUser[i] == arrayFromUser[j])
+                            {
+                                differentValuesOfArray[i] = true;
+                                differentValuesOfArray[j] = true;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine("----------------------");
+
+                for (int i = 0; i < NumberOfLoops; i++)
+                {
+                    if (differentValuesOfArray[i] == false)
+                    {
+                        Console.WriteLine(arrayFromUser[i]);
+                    }
+                }
+
+            }
+            Console.ReadLine();
         }
 
 
