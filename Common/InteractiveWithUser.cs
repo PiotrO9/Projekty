d440faac50a1f764a686b03c0632c9199d0b8a1b
praktyki <praktyki@MTA-27>
2020-07-22 12:36:39 +0200

    ADD Class InteractiveWithUser

---
+++ Common/InteractiveWithUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class InteractiveWithUser
+    {
+        public static int GetIntFromUser()
+        {
+            Console.WriteLine("Wprowadź liczbę");
+            int loops;
+
+            while (!int.TryParse(Console.ReadLine(), out loops))
+            {
+                Console.WriteLine("Wprowadzony ciąg nie jest liczbą");
+            }
+
+            return loops;
+        }
+
+        public static int[] GetIntArray()
+        {
+            Console.WriteLine("Tworzenie nowej tablicy");
+            int loops = GetIntFromUser();
+
+            if (loops <= 0)
+            {
+                Console.WriteLine("Wprowadzono niepoprawną wielkość talbicy");
+                return new int[0];
+            }
+
+            int[] inputArray = new int[loops];
+
+            for (int i = 0; i < loops; i++)
+            {
+                inputArray[i] = GetIntFromUser();
+            }
+
+            return inputArray;
+        }
+    }
+}
---
+++ www.w3resource.com_Array_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loops = InteractiveWithUser.GetIntFromUser();
+
+
+        }
+
+
+    }
+}
