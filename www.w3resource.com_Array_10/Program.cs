ab7b38f7bbbf88bd73dccd206c5d13615654cf67
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-23 09:18:13 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array_10/Program.cs
@@ -0,0 +1,74 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = InteractiveWithUser.GetIntArray();
+
+            int evenElements = 0;
+            int oddElements = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 == 0)
+                {
+                    evenElements++;
+                }
+                else
+                {
+                    oddElements++;
+                }
+            }
+
+            int[] evenArray = new int[evenElements];
+            int[] oddArray = new int[oddElements];
+
+            int j = 0;
+            int k = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 == 0)
+                {
+                    evenArray[j] = myArray[i];
+                    j++;
+                }
+            }
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 != 0)
+                {
+                    oddArray[k] = myArray[i];
+                    k++;
+                }
+            }
+
+            Console.Write("Parzyste liczby to: ");
+
+            for (int i = 0; i < evenArray.Length; i++)
+            {
+                Console.Write(evenArray[i] + " ");
+            }
+
+            Console.Write("\n");
+            Console.Write("Nieparzysteliczby to: ");
+
+            for (int i = 0; i < oddArray.Length; i++)
+            {
+                Console.Write(oddArray[i] + " ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
