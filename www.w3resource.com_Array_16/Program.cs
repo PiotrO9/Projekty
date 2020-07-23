5d5f80ad0b194cb229fd9d094741a31377b2cab9
praktyki <praktyki@MTA-27>
2020-07-23 14:34:27 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array_16/Program.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = InteractiveWithUser.GetIntArray();
+
+            firstArray = Sorting.sortingByDescending(firstArray);
+
+            Console.WriteLine("Druga największa liczba to: " + firstArray[1]);
+            Console.ReadLine();
+        }
+    }
+}
