caa6231aa5bea6124964d16f0758e0cd19b83fa4
praktyki <praktyki@MTA-27>
2020-07-23 13:45:57 +0200

    Zadanie skońcozne

---
+++ www.w3resource.com_Array_15/Program.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = InteractiveWithUser.GetIntArray();
+
+            List<int> firstList = new List<int>();
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                firstList.Add(firstArray[i]);
+            }
+
+            int inputIndex = InteractiveWithUser.GetIntFromUser("Wprowadź Index ");
+
+            Console.WriteLine("Przed usunięciem ");
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                Console.Write(firstList[i] + " ");
+            }
+
+            firstList.RemoveAt(inputIndex);
+
+            Console.WriteLine("Po usunięciu ");
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                Console.Write(firstList[i] + " ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
