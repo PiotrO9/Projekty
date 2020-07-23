f51fce121ec566a431e2518c9b6fd4b207d07e85
praktyki <praktyki@MTA-27>
2020-07-23 14:40:56 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array_17/Program.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = InteractiveWithUser.GetIntArray();
+
+            firstArray = Sorting.sortingByAscending(firstArray);
+
+            Console.WriteLine("Druga najmniejsza liczba to " + firstArray[1]);
+            Console.ReadLine();
+        }
+    }
+}
