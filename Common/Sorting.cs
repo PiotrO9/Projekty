29357bb7ca236007f02c293484687494667202ef
praktyki <praktyki@MTA-27>
2020-07-23 11:56:58 +0200

    ADD sorting by ascending and descending

---
+++ Common/Sorting.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Sorting
+    {
+        public static int[] sortingByAscending(int[] mainArray, int number = 0)
+        {
+            Array.Sort(mainArray);
+
+            if (mainArray.Length < 0)
+            {
+                Console.WriteLine("Nieprawidłowy rozmiar tablicy");
+                return mainArray;
+            }
+
+            if (mainArray.Length == 0)
+            {
+                Console.WriteLine("Sortowanie tablicy nie możliwe, długość wynosi 0");
+            }
+            else if (number > 0)
+            {
+
+                printingArray(mainArray);
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("Sortowanie przebiegło pomyślnie");
+            }
+
+            return mainArray;
+        }
+
+        public static int[] sortingByDescending(int[] mainArray, int number = 0)
+        {
+            Array.Sort(mainArray);
+            Array.Reverse(mainArray);
+
+            if (mainArray.Length < 0)
+            {
+                Console.WriteLine("Nieprawidłowy rozmiar tablicy");
+                return mainArray;
+            }
+
+            if (mainArray.Length == 0)
+            {
+                Console.WriteLine("Sortowanie tablicy nie możliwe, długość wynosi 0");
+            }
+            else if (number > 0)
+            {
+                printingArray(mainArray);
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("Sortowanie przebiegło pomyślnie");
+            }
+
+            return mainArray;
+        }
+
+        public static void printingArray(int[] arrayToPrint)
+        {
+            Console.WriteLine("\n");
+            for (int i = 0; i < arrayToPrint.Length; i++)
+            {
+                Console.Write(arrayToPrint[i]);
+            }
+        }
+
+    }
+}
