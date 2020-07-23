ef0ba021f9b9e2d991920ed8403c69924ab43998
praktyki <praktyki@MTA-27>
2020-07-23 11:31:01 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array_11/Program.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = InteractiveWithUser.GetIntArray();
+
+            Array.Sort(firstArray);
+
+            Console.WriteLine("Elementy posortowane rosnąco");
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                Console.Write(firstArray[i] + " ");
+            }
+
+            Array.Reverse(firstArray);
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                Console.Write(firstArray[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
