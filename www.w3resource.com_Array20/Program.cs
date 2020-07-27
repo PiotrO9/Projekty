003366c740f6f2859cc3bce4e9c1c8744a47ed32
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-27 09:20:10 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array20/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rozmiar macierzy kwadratowej: ");
+
+            int sizeOfMatrice = int.Parse(Console.ReadLine());
+
+            int[,] firstArray = new int[sizeOfMatrice, sizeOfMatrice];
+
+            int[,] secoundArray = new int[sizeOfMatrice, sizeOfMatrice];
+
+            Console.WriteLine("Wprowadź dane dla pierwszej tabeli: ");
+
+            for (int i = 0; i < sizeOfMatrice; i++)
+            {
+                for (int j = 0; j < sizeOfMatrice; j++)
+                {
+                    Console.WriteLine("Podaj liczbę dla [" + i + "][" + j + "]");
+                    firstArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Wprowadź dane dla drugiej tabeli: ");
+
+            for (int i = 0; i < sizeOfMatrice; i++)
+            {
+                for (int j = 0; j < sizeOfMatrice; j++)
+                {
+                    Console.WriteLine("Podaj liczbę dla [" + i + "][" + j + "]");
+                    secoundArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Pierwsza macierz");
+
+            for (int i = 0; i < sizeOfMatrice; i++)
+            {
+                for (int j = 0; j < sizeOfMatrice; j++)
+                {
+                    Console.Write(firstArray[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("Druga macierz");
+
+            for (int i = 0; i < sizeOfMatrice; i++)
+            {
+                for (int j = 0; j < sizeOfMatrice; j++)
+                {
+                    Console.Write(secoundArray[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+
+            int[,] thirdArray = new int[sizeOfMatrice, sizeOfMatrice];
+
+            for (int i = 0; i < sizeOfMatrice; i++)
+            {
+                for (int j = 0; j < sizeOfMatrice; j++)
+                {
+                    thirdArray[i, j] = firstArray[i, j] - secoundArray[i, j];
+                }
+            }
+
+            Console.WriteLine("Trzecia macierz");
+
+            for (int i = 0; i < sizeOfMatrice; i++)
+            {
+                for (int j = 0; j < sizeOfMatrice; j++)
+                {
+                    Console.Write(thirdArray[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ www.w3resource.com_Array_20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace www.w3resource.com_Array_20
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
