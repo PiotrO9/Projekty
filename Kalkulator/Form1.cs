ac33c305ac110d25bc1e0055c11aa4a8f83bed67
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-07-27 10:45:19 +0200

    .

--- Kalkulator/Form1.cs
+++ Kalkulator/Form1.cs
@@ -13,18 +13,37 @@ namespace Kalkulator
 {
     public partial class Kalkulator : Form
     {
+        enum Operation
+        {
+            Add,
+            Sub,
+            Mul,
+            Div,
+            None
+        }
+
+        bool decimalPointActive = false;
+
+        Operation previousOperation = Operation.None;
+        Operation currentOperation = Operation.None;
 
         public string tempText;
-
-        List<int> numbersList = new List<int>();
-
-        List<char> charsList = new List<char>();
-
-        int numberConnecting = 0;
 
         public Kalkulator()
         {
             InitializeComponent();
+        }
+
+        private void EnableOperatorButtons(bool enable = true)
+        {
+            btnMultiplication.Enabled = enable;
+            btnDivision.Enabled = enable;
+            btnPlus.Enabled = enable;
+            if (!enable)
+            {
+                decimalPointActive = false;
+            }
+            //btnSub.Enabled = enable;
         }
 
         private void btn1_Click(object sender, EventArgs e)
@@ -35,137 +54,27 @@ namespace Kalkulator
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            textBox1.Text += "+";
-
-            numbersList.Add(int.Parse(tempText));
-
-            tempText = "";
-
-            charsList.Add('+');
+            ;
         }
 
         private void btnEquals_Click(object sender, EventArgs e)
         {
-            if (numbersList.Count >= 2 && charsList.Count >= 1)
-            {
-                int numberOfPlus = 0;
-                int numberOfMinus = 0;
-                int numberOfMultiplication = 0;
-                int numberOfDivision = 0;
-
-                List<int> listPlus = new List<int>();
-                List<int> listMinus = new List<int>();
-                List<int> listMultiplication = new List<int>();
-                List<int> listDivision = new List<int>();
-
-                //Dzielenie
-
-                for (int i = 0; i < charsList.Count; i++)
-                {
-                    if (charsList[i] == '/')
-                    {
-                        numberOfDivision++;
-                        listDivision.Add(i);
-                    }
-                }
-
-                //////////////////////////////////////////////////
-
-                for (int i = 0; i < listDivision.Count; i++)
-                {
-                    numbersList[listDivision[i]] = numbersList[listDivision[i]] / numbersList[listDivision[i] + 1];
-                    numbersList.RemoveAt(numbersList[listDivision[i] + 1]);
-
-                    charsList.RemoveAt(listDivision[i]);
-                }
-
-                //Mnożenie
-
-                for (int i = 0; i < charsList.Count; i++)
-                {
-                    if (charsList[i] == '*')
-                    {
-                        numberOfMultiplication++;
-                        listMultiplication.Add(i);
-                    }
-                }
-
-                for (int i = 0; i < listMultiplication.Count; i++)
-                {
-                    numbersList[listMultiplication[i]] = numbersList[listMultiplication[i]] * numbersList[listMultiplication[i] + 1];
-                    numbersList.RemoveAt(numbersList[listMultiplication[i] + 1]);
-
-                    charsList.RemoveAt(listMultiplication[i]);
-                }
-
-                //Odejmowanie
-
-                for (int i = 0; i < charsList.Count; i++)
-                {
-                    if (charsList[i] == '-')
-                    {
-                        numberOfMinus++;
-                        listMinus.Add(i);
-                    }
-                }
-
-                for (int i = 0; i < listMultiplication.Count; i++)
-                {
-                    numbersList[listMinus[i]] = numbersList[listMinus[i]] * numbersList[listMinus[i] + 1];
-                    numbersList.RemoveAt(numbersList[listMinus[i] + 1]);
-
-                    charsList.RemoveAt(listMinus[i]);
-                }
-
-                if (charsList.Count == 0)
-                {
-                    int temp = numbersList[0];
-                    textBox1.Text = temp.ToString();
-                }
-                else
-                //Końcowe dodawanie
-                {
-                    for (int i = 0; i < numbersList.Count; i++)
-                    {
-                        numberConnecting += numbersList[i];
-                    }
-                    textBox1.Text = numberConnecting.ToString();
-                }
-
-            }
+            ;
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-            textBox1.Text += "-";
-
-            numbersList.Add(int.Parse(tempText));
-
-            tempText = "";
-
-            charsList.Add('-');
+            ;
         }
 
         private void btnMultiplication_Click(object sender, EventArgs e)
         {
-            textBox1.Text += "*";
-
-            numbersList.Add(int.Parse(tempText));
-
-            tempText = "";
-
-            charsList.Add('*');
+            ;
         }
 
         private void btnDivision_Click(object sender, EventArgs e)
         {
-            textBox1.Text += "/";
-
-            numbersList.Add(int.Parse(tempText));
-
-            tempText = "";
-
-            charsList.Add('/');
+            ;
         }
     }
 }
