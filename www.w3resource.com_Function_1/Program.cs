e9f1990947dc441986424c4e69f189438242aa00
praktyki <praktyki@MTA-27>
2020-07-30 14:11:10 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Function_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Function_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj napis: ");
+            string text = Console.ReadLine();
+
+            string test;
+
+            int countingSpecialSigns = 0;
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                test = text.Substring(i, 1);
+
+                if (test == " ")
+                {
+                    countingSpecialSigns++;
+                }
+
+            }
+            if (countingSpecialSigns == 0)
+            {
+                Console.WriteLine(text + " nie zawiera spacji");
+            }
+            else
+                Console.WriteLine(text + " zawiera " + countingSpecialSigns + " spacje");
+
+            Console.ReadLine();
+
+        }
+    }
+}
