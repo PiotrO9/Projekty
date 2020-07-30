bb27eba4bb1ba57b13ea2f80be941fe5244205dd
praktyki <praktyki@MTA-27>
2020-07-30 13:45:57 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array31/Program.cs
@@ -0,0 +1,45 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] firstMartix = Matrix.generateMatrix();
+            firstMartix = Matrix.fillingMatrix(firstMartix);
+
+            bool checking = true;
+
+            int size = firstMartix.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if ((i == j && firstMartix[i, j] != 1) || (i != j && firstMartix[i, j] != 0))
+                    {
+                        checking = false;
+                    }
+                }
+            }
+
+            if (checking == true)
+            {
+                Console.WriteLine("Macierz jest macierzą tożsamości");
+            }
+            else
+            {
+                Console.WriteLine("Macierz nie jest macierzą tożsamości");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
