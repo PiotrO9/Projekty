c0fa67f58971296dc2aeab8fc803c6beb904cf66
praktyki <praktyki@MTA-27>
2020-07-30 12:26:44 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Array27/Program.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Array27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] firstMatrix = Matrix.generateMatrix();
+            firstMatrix = Matrix.fillingMatrix(firstMatrix);
+
+            int size = firstMatrix.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (j > i)
+                    {
+                        firstMatrix[i, j] = 0;
+                    }
+                }
+            }
+
+            Matrix.printingMatrix(firstMatrix);
+
+            Console.ReadLine();
+        }
+    }
+}
