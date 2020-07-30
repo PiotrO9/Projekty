313bfff7ab4742b438b1a4a9e9719083c217eac1
praktyki <praktyki@MTA-27>
2020-07-30 14:32:23 +0200

    ..

---
+++ www.w3resource.com_Function_5/Program.cs
@@ -0,0 +1,50 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Function_5
+{
+    class Program
+    {
+        public static int counting(int[] mainArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                sum += mainArray[i];
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int size;
+
+            Console.WriteLine("Wprowadź liczbę elementów w tabeli");
+
+            while (int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.WriteLine("Wprowadzona liczba jest nieprawidłowa, wprowadź ponownie");
+            }
+
+
+            int[] firstArray = new int[size];
+
+            Console.WriteLine("Wprowadzanie elementów do tablicy");
+
+            for (int i = 0; i < size; i++)
+            {
+                firstArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            int result = counting(firstArray);
+
+            Console.WriteLine("Suma elementów w tablicy: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
