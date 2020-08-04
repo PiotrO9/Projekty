5fda6508c39ea17258a185d82b6bb9b2a9e53f68
praktyki <praktyki@MTA-27>
2020-08-04 16:46:45 +0200

    zadanie skończone

---
+++ www.w3resource.com_DateTime_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_DateTime_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+
+            DateTime testDT = new DateTime(2016);
+
+            if (isIntercalary(dt) == true)
+            {
+                Console.WriteLine("Podany rok to rok przestępny");
+            }
+            else
+            {
+                Console.WriteLine("Podany rok nie jest rokiem przestpnym");
+            }
+
+            if (isIntercalary(testDT) == true)
+            {
+                Console.WriteLine("Podany rok to rok przestępny");
+            }
+            else
+            {
+                Console.WriteLine("Podany rok nie jest rokiem przestpnym");
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static bool isIntercalary(DateTime year)
+        {
+            if (year.DayOfYear == 366)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
