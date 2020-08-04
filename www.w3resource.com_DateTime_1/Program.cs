c61b39dda2b4a381273fca851c44790777f49c6f
praktyki <praktyki@MTA-27>
2020-08-04 16:27:29 +0200

    zadanie skończone

---
+++ www.w3resource.com_DateTime_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_DateTime_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2016, 8, 6, 11, 49, 0);
+            Console.WriteLine("Complete date: " + dt.ToString());
+            DateTime dateonly = dt.Date;
+            Console.WriteLine("Short Date: " + dateonly.ToString("d"));
+
+            Console.WriteLine(dateonly.ToString("g"));
+            Console.WriteLine(dateonly.ToString("MM/dd/yyyy HH:mm"));
+
+            Console.ReadLine();
+        }
+    }
+}
