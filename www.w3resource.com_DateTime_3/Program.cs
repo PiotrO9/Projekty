17387fe755861e492d2462b820285074a5334fb3
praktyki <praktyki@MTA-27>
2020-08-04 16:36:09 +0200

    Zadanie skończone

---
+++ www.w3resource.com_DateTime_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_DateTime_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2016, 8, 6);
+
+            Console.WriteLine("Dzień tygodnia: " + dt.DayOfWeek);
+
+            Console.ReadLine();
+        }
+    }
+}
