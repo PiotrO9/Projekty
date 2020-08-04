415197bb1cdcc4c4fc1ebddeee259c0143e56b81
praktyki <praktyki@MTA-27>
2020-08-04 12:08:51 +0200

    Nowy projekt - Digital Clock

---
+++ Digital Clock/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblDayName = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblS = new System.Windows.Forms.Label();
+            this.lblHM = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstOfMesurement = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-3, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(549, 289);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Black;
+            this.tabPage1.Controls.Add(this.lblDayName);
+            this.tabPage1.Controls.Add(this.lblDate);
+            this.tabPage1.Controls.Add(this.lblS);
+            this.tabPage1.Controls.Add(this.lblHM);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(541, 263);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Clock";
+            // 
+            // lblDayName
+            // 
+            this.lblDayName.AutoSize = true;
+            this.lblDayName.Font = new System.Drawing.Font("Nirmala UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDayName.ForeColor = System.Drawing.Color.White;
+            this.lblDayName.Location = new System.Drawing.Point(340, 160);
+            this.lblDayName.Name = "lblDayName";
+            this.lblDayName.Size = new System.Drawing.Size(168, 65);
+            this.lblDayName.TabIndex = 7;
+            this.lblDayName.Text = "Friday";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Nirmala UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(50, 146);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(203, 65);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "JUNE 13";
+            // 
+            // lblS
+            // 
+            this.lblS.AutoSize = true;
+            this.lblS.Font = new System.Drawing.Font("Nirmala UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblS.ForeColor = System.Drawing.Color.White;
+            this.lblS.Location = new System.Drawing.Point(334, 81);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(56, 45);
+            this.lblS.TabIndex = 5;
+            this.lblS.Text = "11";
+            // 
+            // lblHM
+            // 
+            this.lblHM.AutoSize = true;
+            this.lblHM.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHM.ForeColor = System.Drawing.Color.White;
+            this.lblHM.Location = new System.Drawing.Point(153, 27);
+            this.lblHM.Name = "lblHM";
+            this.lblHM.Size = new System.Drawing.Size(202, 86);
+            this.lblHM.TabIndex = 4;
+            this.lblHM.Text = "11.11";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Black;
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.lstOfMesurement);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(541, 263);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Stoper";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(35, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 86);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "00:00:00";
+            // 
+            // lstOfMesurement
+            // 
+            this.lstOfMesurement.FormattingEnabled = true;
+            this.lstOfMesurement.Location = new System.Drawing.Point(389, 17);
+            this.lstOfMesurement.Name = "lstOfMesurement";
+            this.lstOfMesurement.Size = new System.Drawing.Size(141, 225);
+            this.lstOfMesurement.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(543, 285);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Digital Clock";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblDayName;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblS;
+        private System.Windows.Forms.Label lblHM;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstOfMesurement;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Digital Clock/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHM.Text = DateTime.Now.ToString("HH:mm");
+            lblS.Text = DateTime.Now.ToString("ss");
+            lblDate.Text = DateTime.Now.ToString("MMM dd yyyy");
+            lblDayName.Text = DateTime.Now.ToString("dddd");
+        }
+
+    }
+}
