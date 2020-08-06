6e110e9bd18ca04bcb6e2fc2b1c165a5cf3f90db
praktyki <praktyki@MTA-27>
2020-08-06 14:46:03 +0200

    zadanie skończone

---
+++ Encrypter/Encrypter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypter
+{
+    class Encrypter
+    {
+        public static string Encrypt(string word)
+        {
+            int LetterInt = 0;
+            char letter = ' ';
+            string textEncripted = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                LetterInt = (int)word[i] + 1;
+                letter = (char)LetterInt;
+                textEncripted += letter.ToString();
+            }
+
+            return textEncripted;
+        }
+
+        public static string Decrypt(string word)
+        {
+            int LetterInt = 0;
+            char letter = ' ';
+            string textEncripted = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                LetterInt = (int)word[i] - 1;
+                letter = (char)LetterInt;
+                textEncripted += letter.ToString();
+            }
+
+            return textEncripted;
+        }
+
+
+    }
+}
---
+++ Encrypter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string newText = Encrypter.Encrypt("Hola");
+            Console.WriteLine("Text encripted: " + newText);
+            string TextDescripted = Encrypter.Decrypt(newText);
+            Console.WriteLine("Text descripted: " + TextDescripted);
+
+            Console.ReadLine();
+        }
+    }
+}
