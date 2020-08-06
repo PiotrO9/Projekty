4b5cf1e29557596793792d521e4d1c2c5d094fac
praktyki <praktyki@MTA-27>
2020-08-06 14:25:52 +0200

    zadanie skończone

---
+++ Array of objects - table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_of_objects___table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table[] myTables = new Table[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                myTables[i] = new Table(rnd.Next(50, 201), rnd.Next(50, 201));
+                myTables[i].ShowData();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Array of objects - table/Table.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_of_objects___table
+{
+    class Table
+    {
+        private float width;
+        private float height;
+
+        public Table()
+        {
+
+        }
+
+        public Table(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public float Width
+        {
+            set { width = value; }
+            get { return width; }
+        }
+
+        public float Height
+        {
+            set { height = value; }
+            get { return height; }
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Width: {0}, Height: {1}", width, height);
+        }
+    }
+}
