06a8bf5c47ad48285ee028c12cbaa2e3ecf7d918
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-08-08 23:43:22 +0200

    Update

--- Liczenie makroskładników/Form1.Designer.cs
+++ Liczenie makroskładników/Form1.Designer.cs
@@ -28,35 +28,264 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView5 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblT = new System.Windows.Forms.Label();
+            this.lblW = new System.Windows.Forms.Label();
+            this.dodajPosiłekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wyczyśćToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // listView1
             // 
-            this.button1.Location = new System.Drawing.Point(263, 46);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 53);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(243, 83);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcjeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(301, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opcjeToolStripMenuItem
+            // 
+            this.opcjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dodajPosiłekToolStripMenuItem,
+            this.wyczyśćToolStripMenuItem});
+            this.opcjeToolStripMenuItem.Name = "opcjeToolStripMenuItem";
+            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.opcjeToolStripMenuItem.Text = "Opcje";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(12, 187);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(243, 83);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3
+            // 
+            this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3});
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(12, 322);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(243, 83);
+            this.listView3.TabIndex = 3;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView4
+            // 
+            this.listView4.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4});
+            this.listView4.HideSelection = false;
+            this.listView4.Location = new System.Drawing.Point(12, 448);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(243, 83);
+            this.listView4.TabIndex = 4;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView5
+            // 
+            this.listView5.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5});
+            this.listView5.HideSelection = false;
+            this.listView5.Location = new System.Drawing.Point(12, 573);
+            this.listView5.Name = "listView5";
+            this.listView5.Size = new System.Drawing.Size(243, 83);
+            this.listView5.TabIndex = 5;
+            this.listView5.UseCompatibleStateImageBehavior = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl1.Location = new System.Drawing.Point(22, 30);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(89, 20);
+            this.lbl1.TabIndex = 6;
+            this.lbl1.Text = "Śniadanie";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl2.Location = new System.Drawing.Point(12, 155);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(111, 20);
+            this.lbl2.TabIndex = 7;
+            this.lbl2.Text = " || Śniadanie";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl3.Location = new System.Drawing.Point(22, 299);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(52, 20);
+            this.lbl3.TabIndex = 8;
+            this.lbl3.Text = "lunch";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl4.Location = new System.Drawing.Point(22, 425);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(56, 20);
+            this.lbl4.TabIndex = 9;
+            this.lbl4.Text = "Obiad";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl5.Location = new System.Drawing.Point(22, 544);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(67, 20);
+            this.lbl5.TabIndex = 10;
+            this.lbl5.Text = "Kolacja";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(230, 670);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(23, 23);
+            this.btn1.TabIndex = 11;
+            this.btn1.Text = "X";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblB.Location = new System.Drawing.Point(13, 675);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(15, 13);
+            this.lblB.TabIndex = 12;
+            this.lblB.Text = "B";
+            // 
+            // lblT
+            // 
+            this.lblT.AutoSize = true;
+            this.lblT.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblT.Location = new System.Drawing.Point(63, 675);
+            this.lblT.Name = "lblT";
+            this.lblT.Size = new System.Drawing.Size(15, 13);
+            this.lblT.TabIndex = 13;
+            this.lblT.Text = "T";
+            // 
+            // lblW
+            // 
+            this.lblW.AutoSize = true;
+            this.lblW.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblW.Location = new System.Drawing.Point(122, 675);
+            this.lblW.Name = "lblW";
+            this.lblW.Size = new System.Drawing.Size(19, 13);
+            this.lblW.TabIndex = 14;
+            this.lblW.Text = "W";
+            // 
+            // dodajPosiłekToolStripMenuItem
+            // 
+            this.dodajPosiłekToolStripMenuItem.Name = "dodajPosiłekToolStripMenuItem";
+            this.dodajPosiłekToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dodajPosiłekToolStripMenuItem.Text = "Dodaj posiłek";
+            // 
+            // wyczyśćToolStripMenuItem
+            // 
+            this.wyczyśćToolStripMenuItem.Name = "wyczyśćToolStripMenuItem";
+            this.wyczyśćToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.wyczyśćToolStripMenuItem.Text = "Wyczyść";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(419, 449);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(318, 616);
+            this.Controls.Add(this.lblW);
+            this.Controls.Add(this.lblT);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.listView5);
+            this.Controls.Add(this.listView4);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Liczenie kalorii";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcjeToolStripMenuItem;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ListView listView5;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ToolStripMenuItem dodajPosiłekToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wyczyśćToolStripMenuItem;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblT;
+        private System.Windows.Forms.Label lblW;
     }
 }
 
--- Liczenie makroskładników/Form1.cs
+++ Liczenie makroskładników/Form1.cs
@@ -17,9 +17,6 @@ namespace Liczenie_makroskładników
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
-        }
     }
 }
