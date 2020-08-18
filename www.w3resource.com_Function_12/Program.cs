c82c85a10ab7037700e3e883e12018a376bf8205
praktyki <praktyki@MTA-27>
2020-08-18 14:07:52 +0200

    Zadanie skonczone

---
+++ www.w3resource.com_Function_12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Function_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibb(number));
+            Console.ReadLine();
+        }
+
+        static int Fibb(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibb(n - 1) + Fibb(n - 2);
+            }
+        }
+    }
+}
