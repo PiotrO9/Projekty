c4b33e14df87b64b4b17cc32e48217d1843aeefa
praktyki <praktyki@MTA-27>
2020-08-18 13:58:45 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Function_11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Function_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+            Console.ReadLine();
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+    }
+}
