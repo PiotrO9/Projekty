ed11d2365c67b99a4918d6d0d815da6c2e04a90c
praktyki <praktyki@MTA-27>
2020-08-18 13:50:54 +0200

    Zadnie skończone

---
+++ www.w3resource.com_Function_10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Function_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(ConvertAndCalculate(s));
+            Console.ReadLine();
+        }
+
+        static int ConvertAndCalculate(string s)
+        {
+            int length = s.Length;
+
+            int conv = int.Parse(s);
+
+            string[] Array = new string[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Array[i] = s[i].ToString();
+            }
+
+            int temp = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                temp += int.Parse(Array[i]);
+            }
+            return temp;
+        }
+    }
+}
