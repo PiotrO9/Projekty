6546690b970ae23c1a67b25b438366593837881e
praktyki <praktyki@MTA-27>
2020-08-18 13:12:11 +0200

    Zadanie skończone

---
+++ www.w3resource.com_Function_7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Function_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumber = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            int SecoundNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculate(FirstNumber, SecoundNumber));
+            Console.ReadLine();
+        }
+
+        static int Calculate(int f, int s)
+        {
+            if (s == 0)
+            {
+                return 1;
+            }
+            else if (s == 1)
+            {
+                return f;
+            }
+            else
+            {
+                int temp = f;
+                for (int i = 1; i < s; i++)
+                {
+                    temp *= f;
+                }
+                return temp;
+            }
+        }
+    }
+}
