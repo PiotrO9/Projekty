5b0925a2ec059089a6ceb370c7eaabf7efae489d
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-08-22 13:50:38 +0200

    Nowy projekt

---
+++ Tetris/Models/Engines/Field.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris.Models.Enums
+{
+    class Field
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public TypeOfField Type { get; set; }
+
+        public PictureBox Picture { get; set; }
+
+        private TypeOfField _type;
+
+        public int MyProperty
+        {
+            get;
+
+            set;
+        }
+
+        public Field(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+
+            this.Picture = new PictureBox()
+            {
+                Name = $"Picture{x}{y}",
+                Margin = new Padding(0, 0, 0, 0),
+            };
+        }
+    }
+}
---
+++ Tetris/Models/Engines/GameEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris.Models.Enums;
+
+namespace Tetris.Models.Engines
+{
+    class GameEngine
+    {
+        private List<Field> _listOfField = new List<Field>();
+
+        public GameEngine()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    var field = new Field(i, j);
+                    field.Type = TypeOfField.none;
+                    _listOfField.Add(field);
+                }
+            }
+        }
+    }
+}
