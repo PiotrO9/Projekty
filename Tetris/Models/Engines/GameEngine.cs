c191ae0ed733e3158529bffcdf87566045a26732
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-08-24 15:34:18 +0200

    Update

--- Tetris/Models/Engines/GameEngine.cs
+++ Tetris/Models/Engines/GameEngine.cs
@@ -15,6 +15,8 @@ namespace Tetris.Models.Engines
         private List<Field> _listOfField = new List<Field>();
         private List<Field> _oneBlockToMove = new List<Field>();
 
+        int CurrentBlockNumber;
+
         public IEnumerable<PictureBox> Pictures => _listOfField.Select(s => s.Picture);
 
         public GameEngine()
@@ -33,7 +35,7 @@ namespace Tetris.Models.Engines
         public void GeneratingBlock()
         {
             Random rnd = new Random();
-            int rn = 1 /*rnd.Next(1, 6)*/;
+            int rn = rnd.Next(1, 3);
 
             GenerateByNumber(rn);
         }
@@ -61,11 +63,31 @@ namespace Tetris.Models.Engines
                         _oneBlockToMove.Add(field2);
                         _oneBlockToMove.Add(field3);
                         _oneBlockToMove.Add(field4);
+
+                        CurrentBlockNumber = 1;
                         break;
                     }
                 case 2:
                     {
+                        Field field1 = _listOfField.Where(w => w.X == 0 && w.Y == 3).FirstOrDefault();
+                        field1.Type = TypeOfField.greenField;
+                        field1.Active = true;
+                        Field field2 = _listOfField.Where(w => w.X == 0 && w.Y == 4).FirstOrDefault();
+                        field2.Type = TypeOfField.greenField;
+                        field2.Active = true;
+                        Field field3 = _listOfField.Where(w => w.X == 1 && w.Y == 3).FirstOrDefault();
+                        field3.Type = TypeOfField.greenField;
+                        field3.Active = true;
+                        Field field4 = _listOfField.Where(w => w.X == 1 && w.Y == 4).FirstOrDefault();
+                        field4.Type = TypeOfField.greenField;
+                        field4.Active = true;
 
+                        _oneBlockToMove.Add(field1);
+                        _oneBlockToMove.Add(field2);
+                        _oneBlockToMove.Add(field3);
+                        _oneBlockToMove.Add(field4);
+
+                        CurrentBlockNumber = 2;
                         break;
                     }
                 case 3:
@@ -98,34 +120,119 @@ namespace Tetris.Models.Engines
 
         public void Timer_tick()
         {
-            for (int i = 0; i < 4; i++) // Ruch
+            if (CheckingColision(1) == true && CurrentBlockNumber == 1)
             {
-                int TempX;
-                int TempY;
-
-                (TempX, TempY) = GetIndex(i); // Pobranie indexu jeszcze starego pola
-
-                IEnumerable<Field> PrevQuery = _listOfField.Where(w => w.X == TempX && w.Y == TempY);
-
-                foreach (var item in PrevQuery)
+                for (int i = 0; i < 4; i++) // Ruch
                 {
-                    item.Type = TypeOfField.none;
+                    int TempX;
+                    int TempY;
+
+                    (TempX, TempY) = GetIndex(i); // Pobranie indexu jeszcze starego pola
+
+                    IEnumerable<Field> PrevQuery = _listOfField.Where(w => w.X == TempX && w.Y == TempY);
+
+                    foreach (var item in PrevQuery)
+                    {
+                        item.Type = TypeOfField.none;
+                    }
+
+                    IEnumerable<Field> query = _listOfField.Where(w => w.X == TempX + 1 && w.Y == TempY); // Lista z nowym polem po przemieszczeniu
+
+                    foreach (var item in query) // Zamiana property type na blueField czyli zmiana koloru na niebieski
+                    {
+                        item.Type = TypeOfField.blueField;
+                    }
+                    _oneBlockToMove.RemoveAt(i); // Usunięcie oraz dodanie nowego elementu w liście z nowymi współrzędnymi
+                    var field = new Field(TempX + 1, TempY);
+                    field.Type = TypeOfField.blueField;
+                    _oneBlockToMove.Insert(i, field);
+                }
+            }
+            else if (CheckingColision(2) == true && CurrentBlockNumber == 2)
+            {
+                for (int i = 3; i > -1; i--)
+                {
+                    int TempX;
+                    int TempY;
+
+                    (TempX, TempY) = GetIndex(i);
+
+                    IEnumerable<Field> PrevQuery = _listOfField.Where(w => w.X == TempX && w.Y == TempY);
+
+                    foreach (var item in PrevQuery)
+                    {
+                        item.Type = TypeOfField.none;
+                    }
+
+                    IEnumerable<Field> query = _listOfField.Where(w => w.X == TempX + 1 && w.Y == TempY);
+
+                    foreach (var item in query)
+                    {
+                        item.Type = TypeOfField.greenField;
+                    }
+                    _oneBlockToMove.RemoveAt(i);
+                    var field = new Field(TempX + 1, TempY);
+                    field.Type = TypeOfField.greenField;
+                    _oneBlockToMove.Insert(i, field);
+                }
+            }
+            else
+            {
+                foreach (var item in _oneBlockToMove)
+                {
+                    item.Active = false;
                 }
 
-                IEnumerable<Field> query = _listOfField.Where(w => w.X == TempX + 1 && w.Y == TempY); // Lista z nowym polem po przemieszczeniu
-
-                foreach (var item in query) // Zamiana property type na blueField czyli zmiana koloru na niebieski
-                {
-                    item.Type = TypeOfField.blueField;
-                }
-                _oneBlockToMove.RemoveAt(i); // Usunięcie oraz dodanie nowego elementu w liście z nowymi współrzędnymi
-                var field = new Field(TempX + 1, TempY);
-                field.Type = TypeOfField.blueField;
-                _oneBlockToMove.Insert(i, field);
+                _oneBlockToMove.Clear();
+                GeneratingBlock();
             }
         }
 
 
+        // Tutaj koniec,oba warunki do przebudowy
+        public bool CheckingColision(int n) //true - ruch możliwy, false - kolizja czyli ruch niemożliwy
+        {
+            int TempX;
+            int TempY;
+
+            if (n == 1)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    (TempX, TempY) = GetIndex(i);
+                    if (TempX + 1 >= 20)
+                    {
+                        return false; // sprawdzenie czy znajdujemy się na samym dole czyli indexie x == 19
+                    }
+                    Field query = _listOfField.Where(w => w.X == TempX + 1 && w.Y == TempY).FirstOrDefault();
+
+                    if (query.Type == TypeOfField.none)
+                    {
+                        return true; // sprawdzenie czy pole pod nami to pole już zajęte
+                    }
+                }
+
+            }
+            else if (n == 2)
+            {
+                for (int i = 2; i < 4; i++)
+                {
+                    (TempX, TempY) = GetIndex(i);
+                    if (TempX + 1 >= 20)
+                    {
+                        return false;
+                    }
+                    Field query = _listOfField.Where(w => w.X == TempX + 1 && w.Y == TempY).FirstOrDefault();
+
+                    if (query.Type == TypeOfField.none)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
 
     }
 }
