1894f1d7c7a70e541e806013c1722c077c7ccb7e
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-10-03 10:16:29 +0200

    Zadanie szkończone

---
+++ www.w3resource.com_Recursion_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Recursion_3
+{
+    class Program
+    {
+        readonly static int number = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ilość liczb do zsumowania: ");
+
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Recursion(1, 10));
+
+            Console.ReadLine();
+        }
+
+        static int Recursion(int min, int val)
+        {
+            if (val == min)
+            {
+                return val;
+            }
+
+            return val + Recursion(min, val - 1);
+        }
+    }
+}
