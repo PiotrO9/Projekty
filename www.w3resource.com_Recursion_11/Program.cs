c09867e0a05b58acdf3bfcc02ab57e97192f017f
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-10-18 20:42:20 +0200

    Update

---
+++ www.w3resource.com_Recursion_11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Recursion_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj 2 liczby: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int hcf = gcd(n1, n2);
+
+            Console.WriteLine("GCD wynosi: " + hcf);
+            Console.WriteLine("LCM wynosi: " + ((n1 * n2) / hcf));
+
+
+            Console.ReadLine();
+        }
+
+        static int gcd(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                return gcd(n2, n1 % n2);
+            }
+        }
+    }
+}
