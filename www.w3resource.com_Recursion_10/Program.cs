11d8c0c87fc687252ee0d7beca25825daa718fe3
PiotrO9 <piotr.ostrowski02@wp.pl>
2020-10-18 20:20:38 +0200

    Update

---
+++ www.w3resource.com_Recursion_10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Recursion_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+
+            int fib = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < fib; i++)
+            {
+                Console.Write(Fibbonaci(i) + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Fibbonaci(int n)
+        {
+            int p = 0;
+            int q = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int temp = p;
+                p = q;
+                q = temp + p;
+            }
+
+            return p;
+        }
+    }
+}
---
+++ www.w3resource.com_Recursion_9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.w3resource.com_Recursion_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+
+            Console.ReadLine();
+
+        }
+
+        static int Factorial(int n, int s = 1)
+        {
+            if (n == 1)
+            {
+                return s;
+            }
+            else
+            {
+                s *= n;
+                n--;
+
+                return Factorial(n, s);
+            }
+        }
+    }
+}
