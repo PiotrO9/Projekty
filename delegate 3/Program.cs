cad1bd45599dd371a3e3bbda880130f71f740939
praktyki <praktyki@MTA-27>
2021-03-08 09:29:40 +0100

    .

---
+++ delegate 3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_3
+{
+    class Program
+    {
+        delegate void Delegate1();
+
+        delegate int Delegate2();
+
+        static void Main(string[] args)
+        {
+            Delegate1 del = text1;
+            del += text2;
+            del += text3;
+            del -= text2;
+
+            del();
+
+            Delegate2 del2 = number1;
+
+            int value = del2();
+
+            Console.WriteLine(value.ToString());
+
+            del2 += number2;
+            value = del2();
+
+            Console.WriteLine(value.ToString());
+
+            Console.ReadLine();
+        }
+
+        static void text1()
+        {
+            Console.WriteLine("text 1");
+        }
+
+        static void text2()
+        {
+            Console.WriteLine("text 2");
+        }
+
+        static void text3()
+        {
+            Console.WriteLine("text 3");
+        }
+
+        static int number1()
+        {
+            return 7;
+        }
+        static int number2()
+        {
+            return 3;
+        }
+    }
+}
