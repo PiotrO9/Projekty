9389e786f353991b302a611033e9bcb1ea704345
PiotrO9 <piotr.ostrowski2020@outlook.com>
2021-05-15 17:28:32 +0200

    ADD NWD algorithm

---
+++ Euklides Algorithm/NWD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euklides_Algorithm
+{
+    static class NWD
+    {
+        public static int Method(int n,int m)
+        {
+            while(m != 0)
+            {
+                int temp = m;
+                m = n % m;
+                n = temp;
+            }
+
+            return n;
+        }
+    }
+}
---
+++ Euklides Algorithm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euklides_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj 1 liczbeę: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj 2 liczbeę: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Największy wspólny dzielnik to: " + NWD.Method(n1,n2));
+            Console.ReadLine();
+
+        }
+    }
+}
