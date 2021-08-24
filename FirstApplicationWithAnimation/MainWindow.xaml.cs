bd9dcfbf044f8444a41e1ea3612f2d69e7f10446
PiotrO9 <piotr.ostrowski2020@outlook.com>
2021-08-24 10:45:07 +0200

    .

---
+++ FirstApplicationWithAnimation/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstApplicationWithAnimation
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Timer timer { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SystemSounds.Asterisk.Play();
+            
+        }
+
+        
+    }
+}
---
+++ FirstApplicationWithAnimations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstApplicationWithAnimations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
