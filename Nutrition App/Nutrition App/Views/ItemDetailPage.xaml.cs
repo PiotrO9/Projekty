7304e0d292d023d48ab8d1844d9702d0e31bee10
PiotrO9 <piotr.ostrowski2020@outlook.com>
2021-12-03 08:44:24 +0100

    Initial commit

---
+++ Nutrition App/Nutrition App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Nutrition_App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Nutrition_App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Nutrition App/Nutrition App/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Nutrition_App.Models;
+using Nutrition_App.ViewModels;
+using Nutrition_App.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nutrition_App.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
