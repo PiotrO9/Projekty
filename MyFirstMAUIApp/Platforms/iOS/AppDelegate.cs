1f3c0fc788934479e1e35b49dbca2bd01653ccc2
Piotr Ostrowski <Piotr.ostrowski@mta.com.pl>
2022-03-22 11:42:56 +0100

    Update - Starting MAUI first project

---
+++ MyFirstMAUIApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Android.Runtime;
+using Foundation;
+
+namespace MyFirstMAUIApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
