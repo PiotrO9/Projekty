c0234cc9a724eb4453f742378893f92915f032d7
PiotrO9 <piotr.ostrowski2020@outlook.com>
2022-03-31 16:31:28 +0200

    ADD - Console project to test online DataBase connection and operations

---
+++ FirstDataBaseConsoleApp/DataBaseInfo.cs
@@ -0,0 +1,17 @@
+﻿using MySqlConnector;
+using MySql.Data.MySqlClient;
+
+namespace FirstDataBaseConsoleApp
+{
+    public class DataBaseInfo
+    {
+        public MySql.Data.MySqlClient.MySqlConnection con { get; set; }
+        public DataBaseInfo()
+        {
+            con = new MySql.Data.MySqlClient.MySqlConnection
+            {
+                ConnectionString = "SERVER= sql11.freemysqlhosting.net;PORT=3306;DATABASE=sql11482763;UID=sql11482763;PASSWORD=4eMXmQDWdU"
+            };
+        }
+    }
+}
---
+++ FirstDataBaseConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDataBaseConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBaseInfo dataBaseInfo = new DataBaseInfo();
+
+            string query = "INSERT INTO `Użytkownicy`(`ID`, `Imie`, `Nazwisko`, `Wiek`) VALUES('1,Jan,Kowalski,23')";
+
+            try
+            {
+                dataBaseInfo.con.Open();
+
+                Console.WriteLine("Połączono z bazą danych");
+
+                MySqlCommand cmd = new MySqlCommand(query,dataBaseInfo.con);
+                int value = cmd.ExecuteNonQuery();
+
+                dataBaseInfo.con.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
