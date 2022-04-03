e0a30ecb42d1907f4b98f8eacd85eb334f630cc7
PiotrO9 <piotr.ostrowski2020@outlook.com>
2022-04-03 11:55:35 +0200

    Add - Simple project to test email verify with regex

---
+++ FirstRegexApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FirstRegexApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string RegularExpresion = @"^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+\.?[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ0-9]+@[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+\.[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]{2,3}$";
+            Regex regex = new Regex(RegularExpresion);
+
+            Console.WriteLine("Podaj maila do sprawdzania: ");
+            string inputMail = Console.ReadLine();
+
+            if (regex.IsMatch(inputMail))
+                Console.WriteLine("Poprawny e-mail");
+            else
+                Console.WriteLine("Niepoprawny e-mail");
+
+            Console.ReadLine();
+        }
+    }
+}
